057e8658d26f277cf53b4cef54498c3b86c764d0
pifopi <dottel.gael@gmail.com>
2021-05-14 15:48:18 +0200

    rename the exe file

--- build.cmd
+++ build.cmd
@@ -3,9 +3,9 @@ pushd %~dp0
 mkdir bin
 cd bin
 cmake ../src -G "Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=C:/Qt/Qt5.12.10/5.12.10/msvc2017_64
-MSBuild.exe .\main.vcxproj /p:Configuration=Debug
-MSBuild.exe .\main.vcxproj /p:Configuration=Release
-MSBuild.exe .\main.vcxproj /p:Configuration=RelWithDebInfo
-MSBuild.exe .\main.vcxproj /p:Configuration=MinSizeRel
+MSBuild.exe .\chess.vcxproj /p:Configuration=Debug
+MSBuild.exe .\chess.vcxproj /p:Configuration=Release
+MSBuild.exe .\chess.vcxproj /p:Configuration=RelWithDebInfo
+MSBuild.exe .\chess.vcxproj /p:Configuration=MinSizeRel
 
 popd
