6b1bef683cc90a25b44032073663a9d15a05eb76
pifopi <dottel.gael@gmail.com>
2021-07-11 19:17:07 +0200

    prepare clang tidy integration

--- build.cmd
+++ build.cmd
@@ -2,7 +2,8 @@ pushd %~dp0
 
 mkdir bin
 cd bin
-cmake ../src -G "Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=C:/Qt/Qt5.12.10/5.12.10/msvc2017_64
+cmake ../src/ -G "Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=C:/Qt/6.2.0/msvc2019_64
+REM -T ClangCL
 MSBuild.exe .\chess.vcxproj /p:Configuration=Debug
 MSBuild.exe .\chess.vcxproj /p:Configuration=Release
 MSBuild.exe .\chess.vcxproj /p:Configuration=RelWithDebInfo
